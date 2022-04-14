d445bbe9e2a5df242840946a439448a9f5fb0cc9
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-04-14 12:28:19 +0700

    try updating socket.io

--- src/main.ts
+++ src/main.ts
@@ -4,106 +4,167 @@ import { Server } from 'socket.io';
 import { expandDims } from "@tensorflow/tfjs";
 import { NormalizedLandmarkList, NormalizedLandmarkListList } from '@mediapipe/hands';
 
-import { loadModel, predictLetter, predictWord } from './predict';
+import { loadModel, predictIllness, predictLetter, predictNumber, predictWord } from './predict';
 
-console.log('Starting ...');
-
-const ws_port = process.env.PORT || 3001;
-const wsHttpServer = createServer();
-const io = new Server(wsHttpServer, {
-	cors: {
-		origin: "*"
-	}, path: '/'
-});
-
-interface holisResult {
-	pose: NormalizedLandmarkList;
-	face: NormalizedLandmarkList;
-	left: NormalizedLandmarkList;
-	right: NormalizedLandmarkList;
-}
-
-const sessionConfident = new Map<string, string[]>();
-const lastPrediction = new Map<string, string>();
-const frameCoordinates = new Map<string, number[][]>(); // store last 30 frame results from Holistic model
-
-io.on('connection', async socket => {
-	console.log(`${new Date().toLocaleTimeString('th', { hour12: false })} [CONNECTED] ${socket.id} (${(await io.allSockets()).size} connected) IP: ${socket.handshake.address}`);
-	sessionConfident.set(socket.id, []);
-	frameCoordinates.set(socket.id, []);
-	socket.on('disconnect', async reason => {
-		console.log(`${new Date().toLocaleTimeString('th', { hour12: false })} [DISCONNECTED] ${socket.id} (${(await io.allSockets()).size} connected) Reason: ${reason}`);
-		sessionConfident.delete(socket.id);
-		lastPrediction.delete(socket.id);
-		frameCoordinates.delete(socket.id);
-	});
-
-	// socket.on('frame', (frame: HTMLVideoElement) => {
-	// 	console.log(frame.width);
-	// });
-
-	socket.on('holis', (result: holisResult) => {
-		// console.log('received');
-		// Format data
-		const combined = [];
-		// console.log(result.pose.length, result.face.length, result.left.length, result.right.length);
-		for (const landmark of result.pose) {
-			combined.push(landmark.x);
-			combined.push(landmark.y);
-			combined.push(landmark.z);
-			combined.push(landmark.visibility);
-		}
-		for (const landmark of result.face) {
-			combined.push(landmark.x);
-			combined.push(landmark.y);
-			combined.push(landmark.z);
-		}
-		for (const landmark of result.left) {
-			combined.push(landmark.x);
-			combined.push(landmark.y);
-			combined.push(landmark.z);
-		}
-		for (const landmark of result.right) {
-			combined.push(landmark.x);
-			combined.push(landmark.y);
-			combined.push(landmark.z);
-		}
-
-		// Predict
-		let frames = frameCoordinates.get(socket.id);
-		frames.push(combined);
-		frames = frames.slice(-30);
-		frameCoordinates.set(socket.id, frames);
-		if (frames.length < 30) return;
-
-		const prediction = predictWord(expandDims(frames));
-		console.log(prediction);
-		if (!prediction) return;
-		const { word, confidence } = prediction;
-
-		socket.emit('word', { word, confidence });
-
-	});
-
-
-	socket.on('mh', (landmarks_list: NormalizedLandmarkListList) => {
-		// Format data
-		const all_hand_pos = [];
-		for (const landmarks of landmarks_list) {
-			for (const joint of landmarks) {
-				all_hand_pos.push(joint.x);
-				all_hand_pos.push(joint.y);
-				all_hand_pos.push(joint.z);
-			}
-		}
-
-		// Predict
-		const prediction = predictLetter(expandDims(all_hand_pos));
-		if (!prediction) return;
-		socket.emit('letter', prediction);
-	});
-});
-
+console.log('Loading models ...');
 loadModel().then(() => {
-	wsHttpServer.listen(ws_port, () => console.log('Listening for socket.io on port ' + ws_port));
+
+	const ws_port = +process.env.PORT || 3001;
+	console.log('Starting socket.io server on port ' + ws_port + ' ...');
+	const io = new Server(ws_port, {
+		cors: {
+			origin: "*"
+		}, path: '/',
+	});
+	console.log('socket.io started on port ' + ws_port);
+
+	interface holisResult {
+		pose: NormalizedLandmarkList;
+		face: NormalizedLandmarkList;
+		left: NormalizedLandmarkList;
+		right: NormalizedLandmarkList;
+	}
+
+	const sessionConfident = new Map<string, string[]>();
+	const lastPrediction = new Map<string, string>();
+	const wordFrames = new Map<string, number[][]>(); // store last 30 frame results from Holistic model
+	const illFrames = new Map<string, number[][]>(); // store last 30 frame results from Holistic model
+
+	io.on('connection', async socket => {
+		console.log(`${new Date().toLocaleTimeString('th', { hour12: false })} [CONNECTED] ${socket.id} (${(await io.allSockets()).size} connected) IP: ${socket.handshake.address}`);
+		sessionConfident.set(socket.id, []);
+		wordFrames.set(socket.id, []);
+		illFrames.set(socket.id, []);
+		socket.on('disconnect', async reason => {
+			console.log(`${new Date().toLocaleTimeString('th', { hour12: false })} [DISCONNECTED] ${socket.id} (${(await io.allSockets()).size} connected) Reason: ${reason}`);
+			sessionConfident.delete(socket.id);
+			lastPrediction.delete(socket.id);
+			wordFrames.delete(socket.id);
+			illFrames.delete(socket.id);
+		});
+
+		// socket.on('frame', (frame: HTMLVideoElement) => {
+		// 	console.log(frame.width);
+		// });
+
+		socket.on('holis_word', (result: holisResult) => {
+			// console.log('received');
+			// Format data
+			const combined = [];
+			// console.log(result.pose.length, result.face.length, result.left.length, result.right.length);
+			for (const landmark of result.pose) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+				combined.push(landmark.visibility);
+			}
+			for (const landmark of result.face) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+			for (const landmark of result.left) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+			for (const landmark of result.right) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+
+			// Predict
+			let frames = wordFrames.get(socket.id);
+			frames.push(combined);
+			frames = frames.slice(-30);
+			wordFrames.set(socket.id, frames);
+			if (frames.length < 30) return;
+
+			const prediction = predictWord(expandDims(frames));
+			console.log(prediction);
+			if (!prediction) return;
+			const { word, confidence } = prediction;
+
+			socket.emit('word', { word, confidence });
+
+		});
+
+		socket.on('holis_ill', (result: holisResult) => {
+			// Format data
+			const combined = [];
+			// console.log(result.pose.length, result.face.length, result.left.length, result.right.length);
+			for (const landmark of result.pose) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+				combined.push(landmark.visibility);
+			}
+			for (const landmark of result.face) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+			for (const landmark of result.left) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+			for (const landmark of result.right) {
+				combined.push(landmark.x);
+				combined.push(landmark.y);
+				combined.push(landmark.z);
+			}
+
+			// Predict
+			let frames = illFrames.get(socket.id);
+			frames.push(combined);
+			frames = frames.slice(-30);
+			illFrames.set(socket.id, frames);
+			if (frames.length < 30) return;
+
+			const prediction = predictIllness(expandDims(frames));
+			console.log(prediction);
+			if (!prediction) return;
+			const { word, confidence } = prediction;
+
+			socket.emit('word', { word, confidence });
+
+		});
+
+		socket.on('mh_letter', (landmarks_list: NormalizedLandmarkListList) => {
+			// Format data
+			const all_hand_pos = [];
+			for (const landmarks of landmarks_list) {
+				for (const joint of landmarks) {
+					all_hand_pos.push(joint.x);
+					all_hand_pos.push(joint.y);
+					all_hand_pos.push(joint.z);
+				}
+			}
+
+			// Predict
+			const prediction = predictLetter(expandDims(all_hand_pos));
+			if (!prediction) return;
+			socket.emit('letter', prediction);
+		});
+
+		socket.on('mh_num', (landmarks_list: NormalizedLandmarkListList) => {
+			// Format data
+			const all_hand_pos = [];
+			for (const landmarks of landmarks_list) {
+				for (const joint of landmarks) {
+					all_hand_pos.push(joint.x);
+					all_hand_pos.push(joint.y);
+					all_hand_pos.push(joint.z);
+				}
+			}
+
+			// Predict
+			const prediction = predictNumber(expandDims(all_hand_pos));
+			if (!prediction) return;
+			socket.emit('letter', prediction);
+		});
+	});
+
 });
--- src/predict.ts
+++ src/predict.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 let holisModel: LayersModel;
 let handModel: LayersModel;
+let numberModel: LayersModel;
+let illnessModel: LayersModel;
 let wordList = [];
+let illnessList = [];
 
 const threshold = 0.95;
 
@@ -19,9 +22,14 @@ const threshold = 0.95;
 export async function loadModel() {
 	holisModel = await loadLayersModel('https://raw.githubusercontent.com/Retaehc-pop/TF_SignLanguage/master/hand/model.json');
 	handModel = await loadLayersModel('https://raw.githubusercontent.com/Retaehc-pop/TF_SignLanguage_handvariation/master/tfjs/model.json');
+	numberModel = await loadLayersModel('https://raw.githubusercontent.com/Retaehc-pop/TF_Signlanguage_number/master/number/model.json');
+	illnessModel = await loadLayersModel('https://raw.githubusercontent.com/Retaehc-pop/TF_Signlanguage_sickness/master/illness/model.json');
 	wordList = (await axios.get('https://raw.githubusercontent.com/Retaehc-pop/TF_SignLanguage/master/label_map.txt')).data.split('\n').map(word => word.split(':')[0]);
+	illnessList = (await axios.get('https://raw.githubusercontent.com/Retaehc-pop/TF_Signlanguage_sickness/master/label_map.txt')).data.split('\n').map(word => word.split(':')[0]);
 	if (!holisModel) throw 'Holistic model not loaded';
 	if (!handModel) throw 'Hand model not loaded';
+	if (!numberModel) throw 'Number model not loaded';
+	if (!illnessModel) throw 'Illness model not loaded';
 }
 
 export function predictWord(input: Tensor<Rank> | Tensor<Rank>[]) {
@@ -36,6 +44,22 @@ export function predictWord(input: Tensor<Rank> | Tensor<Rank>[]) {
 		return;
 	}
 }
+
+export function predictIllness(input: Tensor<Rank> | Tensor<Rank>[]) {
+	let prediction: Float32Array;
+	try {
+		prediction = (<Tensor<Rank>>illnessModel.predict(input)).dataSync() as Float32Array;
+		console.log(prediction);
+		const max = prediction.reduce((max, cur) => max = cur > max ? cur : max, 0);
+		if (max < threshold) return;
+		return { word: illnessList[prediction.indexOf(max)], confidence: max };
+	} catch (err) {
+		console.log('gonna warn');
+		console.warn(`Warning: ${err}`);
+		return;
+	}
+}
+
 export function predictLetter(input: Tensor<Rank> | Tensor<Rank>[]) {
 	let prediction: Float32Array;
 	try {
@@ -50,4 +74,15 @@ export function predictLetter(input: Tensor<Rank> | Tensor<Rank>[]) {
 	}
 }
 
-
+export function predictNumber(input: Tensor<Rank> | Tensor<Rank>[]) {
+	let prediction: Float32Array;
+	try {
+		prediction = (<Tensor<Rank>>numberModel.predict(input)).dataSync() as Float32Array;
+		const max = prediction.reduce((max, cur) => max = cur > max ? cur : max, 0);
+		if (max < threshold) return;
+		return { letter: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9][prediction.indexOf(max)], confidence: max };
+	} catch (err) {
+		console.warn(`Warning: ${err}`);
+		return;
+	}
+}
